98d3b64d960b41d854410ca84a3bdc7ac82ea899
谢琴 邓 <dxq2019@hotmail.com>
2020-12-31 18:23:55 +0800

    ...

--- src/components/RetailOrderList.vue
+++ src/components/RetailOrderList.vue
@@ -406,35 +406,6 @@
        </el-table-column>
      </el-table>
     </el-form>
-    </el-form>
-
-    <!--数据表格-->
-    <el-table
-        ref="multipleTable"
-        :data="tableData"
-        tooltip-effect="dark"
-        style="width: 100%"
-        @selection-change="handleSelectionChange">
-      <el-table-column
-          type="selection"
-          width="55">
-      </el-table-column>
-      <el-table-column
-          label="日期"
-          width="120">
-        <template slot-scope="scope">{{ scope.row.date }}</template>
-      </el-table-column>
-      <el-table-column
-          prop="name"
-          label="姓名"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="address"
-          label="地址"
-          show-overflow-tooltip>
-      </el-table-column>
-    </el-table>
   </el-container>
 </template>
 
